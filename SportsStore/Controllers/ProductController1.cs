ec2b7b49b95c136e3718d38e53198c05c208a385
soulflysk <soulflysk@outlook.co.th>
2020-09-06 21:28:15 +0700

    Add project files.

---
+++ SportsStore/Controllers/ProductController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List() => View(repository.Products);
+    }
+}
---
+++ SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+namespace SportsStore.Models
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
